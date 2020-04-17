43e9e73a2dddf08ed3f1f60b7730a4874681ec17
Yoga Hanggara <yohang88@gmail.com>
2020-04-18 01:39:59 +0700

    fix Area relationship with Hospital and User
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- app/Area.php
+++ app/Area.php
@@ -2,9 +2,6 @@
 
 namespace App;
 
-use App\User;
-use App\Hospital;
-use App\MedicalCase;
 use Illuminate\Database\Eloquent\Model;
 
 class Area extends Model
@@ -22,33 +19,57 @@ class Area extends Model
         'code_kemendagri' ,
     ];
 
-    public function hospitals()
-    {
-    	return $this->hasMany(Hospital::class);
-    }
-
-    public function medicalCases()
-    {
-    	return $this->hasMany(MedicalCase::class);
-    }
-
-    public function getDinkesCode()
-    {
-        return substr($this->code_kemendagri, 0, 5);
-    }
-
-    public function users()
-    {
-        return $this->hasMany(User::class, 'city_code', 'code_kemendagri');
-    }
-
     public function children()
     {
-    	return $this->hasMany(self::class, 'parent_id');
+        return $this->hasMany(Area::class, 'parent_id');
     }
 
     public function parent()
     {
-    	return $this->belongsTo(self::class, 'id', 'parent_id');
+        return $this->belongsTo(Area::class, 'id', 'parent_id');
+    }
+
+    public function hospitals()
+    {
+        $foreignKey = $this->buildForeignKey();
+
+        return $this->hasMany(Hospital::class, $foreignKey, 'code_kemendagri');
+    }
+
+    public function users()
+    {
+        $foreignKey = $this->buildForeignKey();
+
+        return $this->hasMany(User::class, $foreignKey, 'code_kemendagri');
+    }
+
+    public function medicalCases()
+    {
+        return $this->hasMany(MedicalCase::class);
+    }
+
+    /**
+     * Build foreign key name for has many relation (Hospital, User)
+     * Example:
+     * $area = Area::find(1); // Set Province
+     * $area->users // Get all users within Province
+     *
+     * @return string
+     */
+    protected function buildForeignKey()
+    {
+        if ($this->attributes['depth'] === 1) {
+            return $foreignKey = 'province_code';
+        }
+
+        if ($this->attributes['depth'] === 2) {
+            return $foreignKey = 'city_code';
+        }
+
+        if ($this->attributes['depth'] === 3) {
+            return $foreignKey = 'district_code';
+        }
+
+        return $foreignKey = 'village_code';
     }
 }
